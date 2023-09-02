cb94df561a001631e009c83b0ef208456e76c5cb
Vishalvariya999 <110755112+Vishalvariya999@users.noreply.github.com>
2023-09-02 12:20:46 +0530

    Add router

---
+++ src/Router/AllRoute.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+
+const AllRoute = () => {
+    const data = useRoutes([])
+}
+
+export default AllRoute
---
+++ src/shared/components/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const Header = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getData();
+    }, [])
+    const getData = async () => {
+        const data = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+        const res = await data.json()
+        console.log('res', res)
+        setData(res)
+    }
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((res, i) =>
+                        <tr key={i}>
+                            <th scope="row">{res.id}</th>
+                            <td>{res.title}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Header;
